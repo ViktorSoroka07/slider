54de373dac8d38368d646dab2ff52bea0ff2484b
Viktor Soroka <Viktor_Soroka@epam.com>
2015-05-19 22:01:56 +0300

    investigated work with maps

--- js/app.js
+++ js/app.js
@@ -4,11 +4,17 @@
 
     /*global google, CTC */
 
-    var map = new google.maps.Map(document.getElementById("map_canvas"), {
+    var map = new google.maps.Map(document.getElementById("map-canvas"), {
             mapTypeId: google.maps.MapTypeId.ROADMAP
         }),
         lowBound = null,
+        prev_info_window = false,
+        image_marker = './img/map-icon.png',
         $progress = $('.cs-slider_result'),
+        $buttons = $('.map-btn'),
+        cache_maps = {},
+        markers = [],
+
         slider = new CTC.modules.CustomSlider('#cs-slider', {
             animationCallback: function (currentValue, low_bound) {
                 lowBound = low_bound;
@@ -17,44 +23,131 @@
             }
         }),
 
-        getDataDB = function () {
-            return $.getJSON('./DB/places.json');
+        /**
+         * it get places data from the server or cache object
+         * @param data_name - a name of file to get from the data storage
+         */
+        getDataDB = function (data_name) {
+            //if (data_name in cache_maps) {
+            //    return cache_maps[data_name];
+            //}
+            return $.getJSON('./DB/' + data_name + '.json');
         },
 
+        /**
+         * it adds marker to the map
+         * @param map - a map to add marker in
+         * @param coords - the coordinates of marker
+         * @param title - a title on hover on marker
+         */
         addMarker = function (map, coords, title) {
-            new google.maps.Marker({
+
+            var marker = new google.maps.Marker({
                 position: new google.maps.LatLng(coords.lat, coords.lon),
                 title: title,
                 map: map,
-                animation: google.maps.Animation.DROP
+                info: new google.maps.InfoWindow({
+                    content: '<div class="marker-content">' + title + '</p>'
+                }),
+                icon: image_marker
+            });
+
+            google.maps.event.addListener(marker, 'click', function () {
+                if (prev_info_window) {
+                    prev_info_window.close();
+                }
+                prev_info_window = marker.info;
+                map.panTo(marker.getPosition());
+                marker.info.open(map, marker);
+            });
+
+            markers.push(marker);
+
+        },
+
+        /**
+         * it remove one marker from the map that passed in as a parameter
+         * @param marker - marker to delete
+         */
+        removeMarker = function (marker) {
+            marker.setMap(null);
+        },
+
+        /**
+         * it removes all markers from the map
+         * @param markers {Array} - a collection of markers
+         */
+        removeMarkers = function (markers) {
+            $.each(markers, function () {
+                removeMarker(this);
             });
         },
 
-        initMap = function (minZoom, maxZoom, currentZoom) {
+        /**
+         * it renders map according to the params that were passed in
+         * @param minZoom {Number} - min zoom for a map
+         * @param maxZoom {Number} - a max zoom for a map
+         * @param currentZoom {Number} - a current zoom for a map
+         * @param places {Array} - a collection of places to render on map
+         */
+        renderMap = function (minZoom, maxZoom, currentZoom, places) {
+
+            google.maps.event.addListenerOnce(map, 'bounds_changed', function () {
+                this.setZoom(currentZoom);
+            });
+
+            removeMarkers(markers);
 
             slider.setBounds(minZoom, maxZoom);
 
-            $.when(getDataDB()).then(function (data) {
+            $.when(getDataDB(places)).then(function (data) {
+
                 var bounds = new google.maps.LatLngBounds();
 
+                cache_maps[places] = cache_maps[places] || places;
+
                 $.each(data, function () {
+
                     bounds.extend(new google.maps.LatLng(this.latLon.lat, this.latLon.lon));
                     addMarker(map, this.latLon, this.address1);
-                });
 
-                map.fitBounds(bounds);
+                });
 
                 map.setOptions({
                     minZoom: minZoom,
                     maxZoom: maxZoom
                 });
+
+                map.fitBounds(bounds);
+
+                map.setZoom(currentZoom);
+
                 slider.setPosition(currentZoom - minZoom);
+
             });
         };
 
     google.maps.event.addListener(map, 'zoom_changed', function () {
-        slider.setPosition(+map.getZoom() - map.minZoom);
+
+        slider.setPosition(map.getZoom() - map.minZoom);
+
     });
 
-    initMap(5, 20, 15);
+    google.maps.event.addDomListener(window, "resize", function () {
+
+        var center = map.getCenter();
+        google.maps.event.trigger(map, "resize");
+        map.setCenter(center);
+
+    });
+
+    $buttons.on('click', function () {
+
+        var data = $(this).attr('data-places');
+        renderMap(5, 20, 14, data);
+
+    });
+
+    $buttons.first().trigger('click');
+
 }());
